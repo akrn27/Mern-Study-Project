bff527ba8e95e04b924557e5b897e29a2fbe8578
Akrn27 <akurnia755@gmail.com>
2022-02-01 13:49:05 +0700

    change if/else into ternary operator

--- 2_Arsitektur_NodeJS/coba.js
+++ 2_Arsitektur_NodeJS/coba.js
@@ -1,3 +1,15 @@
 // Synchronous
 
-const getUserSync = require('./src/getUserSync');
+const getUserSync = (id) => {
+    const nama = id === 1 ? 'Andika' : 'Kurnia';
+    return { id, nama}
+};
+
+const userSatu = getUserSync(1);
+console.log(userSatu);
+
+const userDua = getUserSync(2);
+console.log(userDua);
+
+const halo = 'Hello World';
+console.log(halo);
