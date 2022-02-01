da7108d80ba70ed32bf100b759651cbe8635b54e
Akrn27 <akurnia755@gmail.com>
2022-02-01 13:55:00 +0700

    create asynchronous

--- 2_Arsitektur_NodeJS/coba.js
+++ 2_Arsitektur_NodeJS/coba.js
@@ -1,15 +1,23 @@
 // Synchronous
 
-const getUserSync = (id) => {
-    const nama = id === 1 ? 'Andika' : 'Kurnia';
-    return { id, nama}
+// const getUserSync = (id) => {
+//     const nama = id === 1 ? 'Andika' : 'Kurnia';
+//     return { id, nama}
+// };
+
+// const userSatu = getUserSync(1);
+// console.log(userSatu);
+
+// const userDua = getUserSync(2);
+// console.log(userDua);
+
+// const halo = 'Hello World';
+// console.log(halo);
+
+
+// Asynchronous
+const getUser = (id, cb) => {
+    setTimeout(() => {
+
+    }, 1000)
 };
-
-const userSatu = getUserSync(1);
-console.log(userSatu);
-
-const userDua = getUserSync(2);
-console.log(userDua);
-
-const halo = 'Hello World';
-console.log(halo);
