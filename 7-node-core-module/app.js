62af33f1552e631abf284ad3b7c2aac76be6a04e
Akrn27 <akurnia755@gmail.com>
2022-02-06 15:46:52 +0700

    finish send data from app.js to contacts.json

--- 7-node-core-module/app.js
+++ 7-node-core-module/app.js
@@ -36,8 +36,17 @@ const rl = readline.createInterface({
 });
 
 rl.question('Masukkan nama anda : ', (nama) => {
-    rl.question('Masukkan umur anda : ', (umur) => {
-        console.log(`Terimakasih ${nama} Berumur ${umur}`);
+    rl.question('Masukkan no HP anda : ', (noHP) => {
+        const contact = {nama, noHP}
+        const file = fs.readFileSync('data/contacts.json', 'utf8');
+        const contacts = JSON.parse(file);
+
+        contacts.push(contact);
+
+        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+
+        console.log('Terimakasih Sudah Menginput Data')
+
         rl.close();
     })
 })
