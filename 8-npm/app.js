cfefa19f3b25b23b715ae24693a67058c0e3db00
Akrn27 <akurnia755@gmail.com>
2022-02-14 16:42:47 +0700

    created numeric validator

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,5 +1,7 @@
 const validator = require('validator');
 
-console.log(validator.isEmail('akurnia755@gmail.com'));
+// console.log(validator.isEmail('akurnia755@gmail.com'));
 
 console.log(validator.isMobilePhone('081387719307', 'id-ID'));
+
+console.log(validator.isNumeric('11a', 'no_symbols'))
