bd2aabee06fd54558178fbb58f131aacfa850251
Akrn27 <akurnia755@gmail.com>
2022-02-14 17:19:48 +0700

    create chalk using template literal

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,9 +1,11 @@
-const validator = require('validator');
-const chalk = require('chalk');
+const validator = require("validator");
+const chalk = require("chalk");
 
 // console.log(validator.isEmail('akurnia755@gmail.com'));
 // console.log(validator.isMobilePhone('081387719307', 'id-ID'));
 // console.log(validator.isNumeric('93081238103', 'no_symbols'));
 
-console.log(chalk.green('Hell'));
-console.log(chalk.black.bgBlue('Hello World!'));
+// console.log(chalk.green('Hello World!'));
+// console.log(chalk.italic.bgBlue.black('Hello World!'));
+const pesan = chalk`Lorem, ipsum dolor {bgRed sit amet} consectetur {bgGreen.italic.black adipiscing} elit. Ab, rem`;
+console.log(pesan);
