1c0436416d622798a7cf3c969990905204012569
Akrn27 <akurnia755@gmail.com>
2022-02-14 17:05:07 +0700

    finish chalk(bug) and solve it with change chalk to the oldest version

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,8 +1,8 @@
 const validator = require('validator');
+const chalk = require('chalk');
 
 // console.log(validator.isEmail('akurnia755@gmail.com'));
-
 // console.log(validator.isMobilePhone('081387719307', 'id-ID'));
+// console.log(validator.isNumeric('93081238103', 'no_symbols'));
 
-// console.log(validator.isNumeric('93081238103', 'no_symbols'))
-
+console.log(chalk.green('Hell'));
