f652f919b9184303036c040c7dac721894d3474c
Akrn27 <akurnia755@gmail.com>
2022-02-21 10:38:05 +0700

    change callback to promises

---
+++ 10-contact-app/app.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// membuat folder data jika belum ada
+const dirPath = './data';
+if(!fs.existsSync(dirPath)) {
+  fs.mkdirSync(dirPath);
+}
+
+// membuat file contacts.json jika belum ada
+const dataPath = './data/contacts.json';
+if(!fs.existsSync(dataPath)) {
+  fs.writeFileSync(dataPath, '[]', 'utf-8');
+}
+
+const pertanyaan1 = () => {
+  return new Promise((resolve, reject) => {
+    rl.question('Masukkan nama anda: ', (nama) => {
+      resolve(nama);
+    });
+  });
+};
+
+
+
+// rl.question("Masukkan nama anda : ", (nama) => {
+//   rl.question("Masukkan no HP anda : ", (noHP) => {
+//     const contact = { nama, noHP };
+//     const file = fs.readFileSync("data/contacts.json", "utf8");
+//     const contacts = JSON.parse(file);
+
+//     contacts.push(contact);
+
+//     fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+
+//     console.log("Terimakasih Sudah Menginput Data");
+
+//     rl.close();
+//   });
+// })
