7c8d440135e0609d51771f77c1382bfbc104d029
Akrn27 <akurnia755@gmail.com>
2022-04-04 16:06:21 +0700

    builder

--- 10-contact-app/app.js
+++ 10-contact-app/app.js
@@ -1,12 +1,25 @@
 const yargs = require("yargs");
 
-yargs.command(
-  "add",
-  "Menambahkan contact baru",
-  () => {},
-  (argv) => {
-    console.log(argv.nama);
-  }
-);
+yargs.command({
+  command: "add",
+  describe: "Menambahkan contact baru",
+  builder: {
+    nama: {
+      describe: "Nama lengkap",
+      demandOption: true,
+      type: "string",
+    },
+    email: {
+      describe: "Email",
+      demandOption: false,
+      type: "string",
+    },
+    noHP: {
+      describe: 'Nomor Handphone',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+});
 
-yargs.parse()
+yargs.parse();
