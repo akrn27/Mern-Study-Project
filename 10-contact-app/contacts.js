0dcac7a5a20ef18d3db18003dbb2f941c89f8fbe
Akrn27 <akurnia755@gmail.com>
2022-04-04 16:59:40 +0700

    fix noHP error

--- 10-contact-app/contacts.js
+++ 10-contact-app/contacts.js
@@ -14,8 +14,8 @@ if (!fs.existsSync(dataPath)) {
   fs.writeFileSync(dataPath, "[]", "utf-8");
 }
 
-const simpanContact = (nama, email, noHp) => {
-  const contact = { nama, email, noHp };
+const simpanContact = (nama, email, noHP) => {
+  const contact = { nama, email, noHP };
   const file = fs.readFileSync("data/contacts.json", "utf8");
   const contacts = JSON.parse(file);
 
@@ -34,8 +34,11 @@ const simpanContact = (nama, email, noHp) => {
     }
   }
 
-
   // cek no HP
+  if(!validator.isMobilePhone(noHP, 'id-ID')){
+    console.log(chalk.red.inverse.bold('Nomor HP tidak valid!'));
+    return false;
+  }
 
   contacts.push(contact);
 
