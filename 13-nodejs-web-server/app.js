9b106c3abb5765e816906ea1e240ff16ea119c47
Akrn27 <akurnia755@gmail.com>
2022-04-06 19:35:37 +0700

    changes from if/else to switch statement

--- 13-nodejs-web-server/app.js
+++ 13-nodejs-web-server/app.js
@@ -22,14 +22,31 @@ http
         })
 
         const url = req.url;
-        if(url === '/about'){
-            renderHTML('./about.html', res);
-        } else if(url === '/contact'){
-            res.write('<h1>Ini adalah halaman Contact</h1>');
-            res.end();
-        } else {
-            renderHTML('./index.html', res);
+
+        // METHOD 1
+        switch(url) {
+            case '/about':
+                renderHTML('./about.html', res);
+                break;
+            case '/contact':
+                res.write('<h1>Ini adalah halaman Contact</h1>');
+                break;
+            default:
+                renderHTML('./index.html', res);
+                break;
         }
+
+
+        // METHOD 2
+        // if(url === '/about'){
+        //     renderHTML('./about.html', res);
+        // } else if(url === '/contact'){
+        //     res.write('<h1>Ini adalah halaman Contact</h1>');
+        //     res.end();
+        // } else {
+        //     renderHTML('./index.html', res);
+        // }
+
 
     })
     .listen(port, () => {
