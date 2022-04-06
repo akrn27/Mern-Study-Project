e2a74ebf692138041f0c4838778828cec67e6f7e
Akrn27 <akurnia755@gmail.com>
2022-04-06 16:43:06 +0700

    create sections

--- 13-nodejs-web-server/app.js
+++ 13-nodejs-web-server/app.js
@@ -6,8 +6,19 @@ http
         res.writeHead(200, {
             'Content-Type': 'text/html',
         })
-        res.write('Hello World!');
-        res.end();
+
+        const url = req.url;
+        if(url === '/about'){
+            res.write('<h1>Ini adalah halaman About</h1>');
+            res.end();
+        } else if(url === '/contact'){
+            res.write('<h1>Ini adalah halaman Contact</h1>');
+            res.end();
+        } else {
+            res.write('Hello World!');
+            res.end();
+        }
+
     })
     .listen(port, () => {
         console.log(`Server is listening on port ${port}`);
