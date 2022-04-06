3ed98da63b346ed86cc1f763d373c75567d15f0d
Akrn27 <akurnia755@gmail.com>
2022-04-06 16:37:25 +0700

    change content-type from plain to HTML

--- 13-nodejs-web-server/app.js
+++ 13-nodejs-web-server/app.js
@@ -3,6 +3,9 @@ const port = 3000;
 
 http
     .createServer((req, res) => {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+        })
         res.write('Hello World!');
         res.end();
     })
