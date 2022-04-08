ff84cc4c3069549ff08fe49b902a320e582894e1
Akrn27 <akurnia755@gmail.com>
2022-04-08 10:56:22 +0700

    patch all pages

--- 14-express-web-server/app.js
+++ 14-express-web-server/app.js
@@ -1,54 +1,75 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const port = 3000;
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const renderHTML = (path, res) => {
-    fs.readFile(path, (err, data) => {
-        if(err) {
-            res.writeHead(404);
-            res.write('Error: file not found');
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
-};
+app.get('/about', (req, res) => {
+    res.send('Ini adalah Halaman About')
+  })
 
-http
-    .createServer((req, res) => {
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
-        })
+app.get('/contact', (req, res) => {
+    res.send('Ini adalah Halaman Contact')
+})
 
-        const url = req.url;
-
-        // METHOD 1
-        switch(url) {
-            case '/about':
-                renderHTML('./about.html', res);
-                break;
-            case '/contact':
-                renderHTML('./contact.html', res);
-                break;
-            default:
-                renderHTML('./index.html', res);
-                break;
-        }
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
-        // METHOD 2
-        // if(url === '/about'){
-        //     renderHTML('./about.html', res);
-        // } else if(url === '/contact'){
-        //     res.write('<h1>Ini adalah halaman Contact</h1>');
-        //     res.end();
-        // } else {
-        //     renderHTML('./index.html', res);
-        // }
+// const http = require('http');
+// const fs = require('fs');
+
+// const port = 3000;
+
+// const renderHTML = (path, res) => {
+//     fs.readFile(path, (err, data) => {
+//         if(err) {
+//             res.writeHead(404);
+//             res.write('Error: file not found');
+//         } else {
+//             res.write(data);
+//         }
+//         res.end();
+//     });
+// };
+
+// http
+//     .createServer((req, res) => {
+//         res.writeHead(200, {
+//             'Content-Type': 'text/html',
+//         })
+
+//         const url = req.url;
+
+//         // METHOD 1
+//         switch(url) {
+//             case '/about':
+//                 renderHTML('./about.html', res);
+//                 break;
+//             case '/contact':
+//                 renderHTML('./contact.html', res);
+//                 break;
+//             default:
+//                 renderHTML('./index.html', res);
+//                 break;
+//         }
 
 
-    })
-    .listen(port, () => {
-        console.log(`Server is listening on port ${port}`);
-    });
+//         // METHOD 2
+//         // if(url === '/about'){
+//         //     renderHTML('./about.html', res);
+//         // } else if(url === '/contact'){
+//         //     res.write('<h1>Ini adalah halaman Contact</h1>');
+//         //     res.end();
+//         // } else {
+//         //     renderHTML('./index.html', res);
+//         // }
+
+
+//     })
+//     .listen(port, () => {
+//         console.log(`Server is listening on port ${port}`);
+//     });
