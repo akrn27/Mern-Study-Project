a9617dd8182d37434a68a46563bc74d057329688
Akrn27 <akurnia755@gmail.com>
2022-04-17 16:13:00 +0700

    create new folder namely 16-express-middleware

---
+++ 16-express-middleware/app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const expressLayouts = require('express-ejs-layouts');
+const app = express();
+const port = 3000;
+
+// gunakan ejs
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+
+app.get("/", (req, res) => {
+  const mahasiswa = [
+    {
+      nama: "Muhammad Andika K",
+      email: "muhammadandikak@gmail.com",
+    },
+    {
+      nama: "kurnia",
+      email: "kurnia@gmail.com",
+    },
+    {
+      nama: "Muhammad",
+      email: "muhammad@gmail.com",
+    },
+  ];
+
+  res.render("index", { 
+    nama: "Muhammad Andika", 
+    title: "Halaman Home",
+    mahasiswa,
+    layout: 'layouts/main-layout',
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { 
+    layout: 'layouts/main-layout',
+    title: 'Halaman About'
+  });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { 
+    layout: 'layouts/main-layout',
+    title: 'Halaman Contact'
+  });
+});
+
+app.get("/product/:id", (req, res) => {
+  res.send(
+    `Product ID : ${req.params.id} <br> Category : ${req.query.category}`
+  );
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>404</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
