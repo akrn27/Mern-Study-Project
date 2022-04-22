8019ff10e92cb189e7fa3c1cb65a69ae1f73f0c2
Akrn27 <akurnia755@gmail.com>
2022-04-22 09:45:26 +0700

    edit contact file

--- 17-express-contact-app/app.js
+++ 17-express-contact-app/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const expressLayouts = require('express-ejs-layouts');
-const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
@@ -9,16 +8,9 @@ app.set("view engine", "ejs");
 
 // Third-party Middleware
 app.use(expressLayouts);
-app.use(morgan('dev'));
 
 // Built-in middleware
 app.use(express.static('public'));
-
-// Application level middleware
-app.use((req, res, next) => {
-  console.log('Time: ', Date.now());
-  next();
-})
 
 app.get("/", (req, res) => {
   const mahasiswa = [
@@ -61,12 +53,6 @@ app.get("/contact", (req, res) => {
     layout: 'layouts/main-layout',
     title: 'Halaman Contact'
   });
-});
-
-app.get("/product/:id", (req, res) => {
-  res.send(
-    `Product ID : ${req.params.id} <br> Category : ${req.query.category}`
-  );
 });
 
 app.use((req, res) => {
