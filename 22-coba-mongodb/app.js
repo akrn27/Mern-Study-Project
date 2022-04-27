e147de7c96c53821e50c23aa81ed8659d04db925
Akrn27 <akurnia755@gmail.com>
2022-04-27 19:24:15 +0700

    add db

--- 22-coba-mongodb/app.js
+++ 22-coba-mongodb/app.js
@@ -1,9 +1,33 @@
 const { MongoClient } = require('mongodb');
 
-const url = 'mongodb://127.0.0.1:27017';
-const dbName = 'wpu';
+const uri = 'mongodb://127.0.0.1:27017';
+const dbName = 'akrn';
 
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+
+client.connect((error, client) => {
+    if(error) {
+        return console.log('Koneksi gagal!');
+    }
+
+    // pilih database
+    const db = client.db(dbName);
+
+    // Menambahkan 1 data ke collection mahasiswa
+    db.collection('mahasiswa').insertOne(
+        {
+            nama: 'Erik',
+            email: 'erik@gmail.com'
+        },
+        (error, result) => {
+            if(error) {
+                return console.log('gagal menambahkan data!');
+            }
+
+            console.log(result);
+        }
+    )
+})
