3aa53efbe805de64cc22216f0fbe5b6ca06c2d0a
Akrn27 <akurnia755@gmail.com>
2022-05-02 05:10:25 +0700

    add detail contact

--- 23-mongo-contact-app/app.js
+++ 23-mongo-contact-app/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+
 require('./utils/db');
 const Contact = require('./model/contact');
 
@@ -12,6 +16,18 @@ app.set('view engine', 'ejs');
 app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+// Konfigurasi Flash
+app.use(cookieParser('secret'));
+app.use(
+  session({
+    cookie: { maxAge: 6000 },
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
 
 // Halaman Home
 app.get("/", (req, res) => {
@@ -62,6 +78,17 @@ app.get("/", (req, res) => {
       msg: req.flash('msg'),
     });
   });
+
+// Halaman detail contact
+app.get('/contact/:nama', async (req, res) => {
+  const contact = await Contact.findOne({ nama: req.params.nama });
+
+  res.render('detail', {
+    title: 'Halaman Detail Contact',
+    layout: 'layouts/main-layout',
+    contact,
+  });
+});
 
 app.listen(port, () => {
     console.log(`Mongo Contact App | listening at http://localhost:${port}`);
