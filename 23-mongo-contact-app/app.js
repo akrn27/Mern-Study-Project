543dc121e7426063eac8077cfaf74a931c150fd6
Akrn27 <akurnia755@gmail.com>
2022-05-01 16:38:37 +0700

    setup EJS

--- 23-mongo-contact-app/app.js
+++ 23-mongo-contact-app/app.js
@@ -4,6 +4,12 @@ const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 3000;
 
+// Setup EJS
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
 // Halaman Home
 app.get("/", (req, res) => {
     const mahasiswa = [
